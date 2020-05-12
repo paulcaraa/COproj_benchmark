368a4eb26242285f2460f5926ce30df1ff8f6287
Adrian Achim <adrian99achim@gmail.com>
2020-05-12 13:23:44 +0300

    Added a benchmark for the speed of download

--- AppBenchMark/app/src/main/java/com/wemarkbenches/cobenchmarkapp/benchmark/RAMbenchmark/RAMbenchmark.java
+++ AppBenchMark/app/src/main/java/com/wemarkbenches/cobenchmarkapp/benchmark/RAMbenchmark/RAMbenchmark.java
@@ -1,9 +1,21 @@
 package com.wemarkbenches.cobenchmarkapp.benchmark.RAMbenchmark;
 
 import com.wemarkbenches.cobenchmarkapp.benchmark.IBenchmark;
+import java.util.Random;
+
 
 public class RAMbenchmark implements IBenchmark {
-    @Override
+
+    private byte[] _buffer;
+    public  byte[] _target;
+
+    public RAMbenchmark() {
+
+        final int size = 50*1024*1024;
+        final Random random = new Random(0);
+    }
+
+
     public void initialize(Object... param) {
 
     }
@@ -16,6 +28,11 @@ public class RAMbenchmark implements IBenchmark {
     @Override
     public void run() {
 
+        for (long  i = 0; i < 100;i++) {
+
+            _target = new byte[_buffer.length];
+            System.arraycopy(_buffer,0,_target,0,_buffer.length);
+        }
     }
 
     @Override
