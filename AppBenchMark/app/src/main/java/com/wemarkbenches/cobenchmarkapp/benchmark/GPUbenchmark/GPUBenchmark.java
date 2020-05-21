922e7c4b3019becdd08c83cb783438dcd567320b
IoanBasnic <57294224+IoanBasnic@users.noreply.github.com>
2020-05-21 21:47:50 +0300

    The gpu that doesn't work (#7)

--- AppBenchMark/app/src/main/java/com/wemarkbenches/cobenchmarkapp/benchmark/GPUbenchmark/GPUBenchmark.java
+++ AppBenchMark/app/src/main/java/com/wemarkbenches/cobenchmarkapp/benchmark/GPUbenchmark/GPUBenchmark.java
@@ -1,40 +1,50 @@
 package com.wemarkbenches.cobenchmarkapp.benchmark.GPUbenchmark;
 
-import com.wemarkbenches.cobenchmarkapp.benchmark.IBenchmark;
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.os.Handler;
+import android.content.Intent;
+import android.view.View;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class GPUBenchmark implements IBenchmark {
+import com.wemarkbenches.cobenchmarkapp.Activities.GPUActivity;
+import com.wemarkbenches.cobenchmarkapp.R;
+
+public class GPUBenchmark extends AppCompatActivity{
+
+    private GLSurfaceView glView;
+    private Handler mHandler = new Handler();
+    public static int nr = 5;
+
+    // Call back when the activity is started, to initialize the view
     @Override
-    public void initialize(Object... param) {
-
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        glView = new GLSurfaceView(this);           // Allocate a GLSurfaceView
+        glView.setRenderer(new MyGLRenderer(this , nr)); // Use a custom renderer
+        this.setContentView(glView);
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(GPUBenchmark.this, GPUActivity.class);
+                startActivity(intent);
+            }
+        }, 4000); // 4 seconds
+        nr++;
     }
 
+    // Call back when the activity is going into the background
     @Override
-    public void run(Object... param) {
-
+    protected void onPause() {
+        super.onPause();
+        glView.onPause();
     }
 
+    // Call back after onPause()
     @Override
-    public void run() {
-
-    }
-
-    @Override
-    public void clean() {
-
-    }
-
-    @Override
-    public void cancel() {
-
-    }
-
-    @Override
-    public void warmUp() {
-
-    }
-
-    @Override
-    public String getResult() {
-        return null;
+    protected void onResume() {
+        super.onResume();
+        glView.onResume();
     }
 }
